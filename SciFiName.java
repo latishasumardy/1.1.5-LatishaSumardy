db3485724d8edccbf91cd6a99269b2ecafb42ac1
Latisha Sumardy <lsmarty23@gmail.com>
2017-09-12 16:44:35 -0700

    finish

--- SciFiName.java
+++ SciFiName.java
@@ -13,13 +13,13 @@ public class SciFiName
      * All input that you privide should be at least three letters long
      * or the program may crash (but feel free to test this!)
      * 
-     * For best results, user lowercase letters and do not use spaces in your input
+     
      */
     public static void main()
     {
         System.out.println("If you provide me some information I will provide a Science Fiction name for you.");
         System.out.println("Please have all responses  be at least three characters long.");
-        System.out.println("For best results, user lowercase letters with no spaces.\n");
+        
 
         // notice this methd (print) does not print a newline
         System.out.print("Enter your first name: ");
@@ -37,13 +37,19 @@ public class SciFiName
 
         // generate a sciFi name
         String scifiFirst = firstName.substring(0, 3) + lastName.substring(0,2);
+        String firstnameLetter = scifiFirst.substring(0,1).toUpperCase();
+        scifiFirst = firstnameLetter + scifiFirst.substring(1);
         String scifiLast = city.substring(0,2) + school.substring(0,3);
+        String lastnameLetter = scifiLast.substring(0,1).toUpperCase();
+        scifiLast = lastnameLetter + scifiLast.substring(1);
         String scifiName = (scifiFirst + " " + scifiLast).toLowerCase();
         int location = (int)(Math.random() * (relativeName1.length()-1));
         String origin1 = relativeName1.substring(location);
         int location2 = (int)(Math.random() * (relativeName2.length()-1));
         String origin2 = relativeName2.substring(location2);
         String originPlace = (origin1 + origin2).toLowerCase();
+        String originLetter = originPlace.substring(0,1).toUpperCase();
+        originPlace = originLetter + originPlace.substring(1);
         System.out.println("Hello " + scifiFirst + " " + scifiLast + " of " + originPlace+ ". Welcome!");
         
     }
